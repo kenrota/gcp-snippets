9ae3ad4a11149b484b29c04138edc517ff3259c3
Kentaro Tokutomi <kenrota@users.noreply.github.com>
2021-08-18 03:04:27 +0900

    Add `append()` example

--- lang/python/pandas_operations.py
+++ lang/python/pandas_operations.py
@@ -16,10 +16,21 @@ temperatures_2 = pd.DataFrame(
 )
 assert [0, 1, 2] == temperatures_1.index.to_list()
 
-# keep original indices
+
+# append()
+
+# Ignore original indices
+temperatures = temperatures_1.append(temperatures_2, ignore_index=True)
+assert [0, 1, 2] == temperatures_1.index.to_list()
+assert [0, 1, 2, 3, 4, 5] == temperatures.index.to_list()
+
+
+# concat()
+
+# Keep original indices
 temperatures = pd.concat([temperatures_1, temperatures_2])
 assert [0, 1, 2, 0, 1, 2] == temperatures.index.to_list()
 
-# ignore original indices
+# Ignore original indices
 temperatures = pd.concat([temperatures_1, temperatures_2], ignore_index=True)
 assert [0, 1, 2, 3, 4, 5] == temperatures.index.to_list()
