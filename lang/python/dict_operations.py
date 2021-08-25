48c0678d9fb1f7d01f7c489b457ab3e805b3d1e5
Kentaro Tokutomi <kenrota@users.noreply.github.com>
2021-08-25 23:15:17 +0900

    copy(), fromkeys(), get(), pop()

--- lang/python/dict_operations.py
+++ lang/python/dict_operations.py
@@ -14,3 +14,15 @@ assert data == dict(zip(keys, values))
 keys = ['a', 'b', 'c']
 values = [0, 1, 2, 3] # ignore extra value
 assert data == dict(zip(keys, values))
+
+copy = data.copy()
+copy.clear()
+assert {} == copy
+
+assert { 'a': 0, 'b': 0, 'c': 0 } == dict.fromkeys(keys, 0) # initialize by given value
+assert 3 == data.get('d', 3) # return given value if given key does not exist
+assert 2 == data.get('c', 3) # not return given value if given key exists
+
+copy = data.copy()
+assert 0 == copy.pop('a')
+assert { 'b': 1, 'c': 2 } == copy
