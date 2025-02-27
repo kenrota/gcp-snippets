8822cbf4194d498c73383af0220c4e3a76e3c958
Kentaro Tokutomi <16278068+kenrota@users.noreply.github.com>
2025-02-27 16:07:30 +0900

    Hono example

---
+++ gcp/cloudfunctions/hono/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  type = string
+}
+
+variable "prefix" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "service_account" {
+  type = string
+}
